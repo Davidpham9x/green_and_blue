65b2b2c2dfac5bcdda3fb183145703f8250aeee6
DavidPham <DavidPham>
2016-06-26 23:28:28 +0700

    Update more page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,105 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+var gbCompany = window.gbCompany || {}; //global namespace for YOUR gbCompany, Please change gbCompany to your gbCompany name
+
+var isMobile = {
+    isAndroid: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    isBlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    isiOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    isOpera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    isWindows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.isAndroid() || isMobile.isBlackBerry() || isMobile.isiOS() || isMobile.isOpera() || isMobile.isWindows());
+    }
+};
+
+(function($) {
+    gbCompany.Global = {
+        modalSubmitVideo: null,
+
+        init: function() { //initialization code goes here
+            $.support.cors = true;
+            this.initFormElements();
+            this.initMasheadSlider();
+        },
+
+        initFormElements: function() {
+            $('input, textarea').placeholder();
+
+            $(".radio-wrapper .input-radio").each(function() {
+                if ($(this).is(":checked")) {
+                    $('.input-radio[name="' + $(this).attr('name') + '"]').parents(".radio-selected").removeClass("radio-selected");
+                    $(this).parents('.radio-wrapper').addClass("radio-selected");
+                }
+            });
+
+            $(document).on('change', ".radio-wrapper .input-radio", function() {
+
+                $('input[name="' + $(this).attr('name') + '"]').each(function() {
+                    if ($(this).not(':checked')) {
+                        $(this).parent().removeClass("radio-selected");
+                    }
+                });
+
+                if ($(this).is(":checked")) {
+                    $(this).parents('.radio-wrapper').addClass("radio-selected");
+                }
+            });
+
+            //Checkbox Wrapper
+            $('.checkbox-wrapper .input-checkbox').each(function() {
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox-wrapper').addClass('checked');
+                }
+            });
+
+            $(document).on('click', '.checkbox-wrapper .input-checkbox', function() {
+
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox-wrapper').addClass('checked');
+                } else if ($(this).not(':checked')) {
+                    $(this).parents('.checkbox-wrapper').removeClass('checked');
+                }
+            });
+
+            //Select Wrapper
+            $('.select-wrapper').each(function() {
+                if ($(this).find('span').length <= 0) {
+                    $(this).prepend('<span>' + $(this).find('select option:selected').text() + '</span>');
+                }
+            });
+
+            $(document).on('change', '.select-wrapper select', function() {
+                $(this).prev('span').replaceWith('<span>' + $(this).find('option:selected').text() + '</span>');
+            });
+        },
+
+        initMasheadSlider: function() {
+            if ( $('.masthead__banner').length && $('.masthead__banner').children().length > 2 ) {
+                $('.masthead__banner').slick({
+                    infinite: true,
+                    speed: 500,
+                    fade: true,
+                    cssEase: 'linear',
+                    appendArrows: $('.masthead__arrow > .container'),
+                    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i></button>',
+                    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right fa-2x" aria-hidden="true"></i></button>'
+                });
+            }
+        },
+    };
+})(jQuery);
+
+$(document).ready(function() {
+    gbCompany.Global.init();
+    $(document).foundation();
+});
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-// generated on 2016-06-24 using generator-webapp 2.0.0
+// generated on 2016-05-20 using generator-webapp 2.0.0
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
@@ -52,11 +52,11 @@ gulp.task('lint', lint('app/scripts/**/*.js'));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles', 'scripts'], () => {
-  return gulp.src('app/*.html')
+  return gulp.src('.tmp/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    // .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
 
@@ -90,7 +90,14 @@ gulp.task('extras', () => {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
+gulp.task('html-include', function() {
+  return gulp.src('app/*.html')
+      .pipe($.htmlTagInclude())
+      .pipe(gulp.dest('.tmp'))
+      .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', ['html-include', 'styles', 'scripts', 'fonts'], () => {
   browserSync({
     notify: false,
     port: 9000,
@@ -103,11 +110,12 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
   });
 
   gulp.watch([
-    'app/*.html',
+    'app/**/*.html',
     'app/images/**/*',
     '.tmp/fonts/**/*'
   ]).on('change', reload);
 
+  gulp.watch('app/**/*.html', ['html-include']);
   gulp.watch('app/styles/**/*.scss', ['styles']);
   gulp.watch('app/scripts/**/*.js', ['scripts']);
   gulp.watch('app/fonts/**/*', ['fonts']);
@@ -158,10 +166,10 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['html-include', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
-gulp.task('default', ['clean'], () => {
+gulp.task('default', () => {
   gulp.start('build');
 });
